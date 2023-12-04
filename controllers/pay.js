633738c170ef8ea5c8763010e8b11b27ec302636
crecordz <s014451@mail.ru>
2023-12-04 17:48:17 +0300

    check

--- controllers/pay.js
+++ controllers/pay.js
@@ -3,31 +3,31 @@ const request = require('request');
 
 const pay = (req, res) => {
   const paymentId = req.body.id;
-
+  res.send('check')
   const sendPaymentRequest = () => {
-    request.post(
-      {
-        url: 'https://api.yookassa.ru/v3',
-        form: {
-          id: paymentId,
-        },
-      },
-      (err, response) => {
-        if (err) {
-          res.status(500).send({ message: err });
-        } else {
-          // Ответ Юкассы
-          const responseBody = JSON.parse(response.body);
-          console.log(responseBody);
-          if (responseBody.status === '200') {
-            clearInterval(paymentInterval);
-            res.send(responseBody);
-          } else {
-            res.send(responseBody);
-          }
-        }
-      },
-    );
+    // request.post(
+    //   {
+    //     url: 'https://api.yookassa.ru/v3',
+    //     form: {
+    //       id: paymentId,
+    //     },
+    //   },
+    //   (err, response) => {
+    //     if (err) {
+    //       res.status(500).send({ message: err });
+    //     } else {
+    //       // Ответ Юкассы
+    //       const responseBody = JSON.parse(response.body);
+    //       console.log(responseBody);
+    //       if (responseBody.status === '200') {
+    //         clearInterval(paymentInterval);
+    //         res.send(responseBody);
+    //       } else {
+    //         res.send(responseBody);
+    //       }
+    //     }
+    //   },
+    // );
   };
 
   const paymentInterval = setInterval(sendPaymentRequest, 5000);
